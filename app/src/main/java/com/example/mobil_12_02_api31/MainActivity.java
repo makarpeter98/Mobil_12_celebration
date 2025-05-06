10313c44d36dc335d0cb5ad7bf828e4594f969c2
makar <makar.peter98@gmail.com>
2025-05-06 10:27:18 +0200

    Mobil 12 Celebration alap

---
+++ app/src/main/java/com/example/mobil_12_02_api31/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.mobil_12_02_api31;
+
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.example.mobil_12_02_api31.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mPlayer;
+    private ActivityMainBinding binding;
+    private String path;
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    Handler handler = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        //setContentView(R.layout.activity_main);
+    }
+    private void download() {
+
+    }
+
+    public void playSong() {
+
+    }
+}
